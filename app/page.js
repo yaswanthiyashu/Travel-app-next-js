0165b1c7c94502ed9797baa4c19916bf2dc36c3e
yaswanthiyashu <107607008+yaswanthiyashu@users.noreply.github.com>
2024-01-20 11:27:51 +0530

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,18 @@
+import Camp from "@/components/Camp";
+import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
+import Feature from "@/components/Feature"
+
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Feature />
+      <GetApp />
+    </>
+  );
+}
